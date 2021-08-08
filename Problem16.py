be91257f1545d33ac9ec318dce674353fb0e136c
derpen <rizqyassabil@hotmail.com>
2021-08-08 13:47:59 +0700

    easiest problem of my life

---
+++ Problem16.py
@@ -0,0 +1,14 @@
+"""
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 21000?
+"""
+
+def powerdigitsum(power):
+    num = str(2 ** power)
+    total = 0
+    for i in range(0, len(num)):
+        total += int(num[i])
+    return total
+
+print(powerdigitsum(1000))
