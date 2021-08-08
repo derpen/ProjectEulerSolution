a3c22fd1e394620aa76de1521b63c39642e448f1
derpen <rizqyassabil@hotmail.com>
2021-08-08 13:31:42 +0700

    Stole idea from some guy from stack overflow

---
+++ Problem15.py
@@ -0,0 +1,16 @@
+"""
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+https://projecteuler.net/project/images/p015.png
+
+How many such routes are there through a 20×20 grid?
+"""
+
+import math
+
+def latticepaths(size):
+    n = size*2
+    result = math.comb(n, size) # n! / (k! ( n- k )!)
+    return result
+
+print(latticepaths(20))
